2d39809ca50c43339e70d3019c55c6e1ca1002f7
JuanDeve <developerje2024@gmail.com>
2026-04-14 19:55:45 -0500

    feat: initialize root layout with Google Tag Manager and custom fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es">
-      <GoogleTagManager gtmId="GTM-KJJV49ZJ" />
+      <GoogleTagManager gtmId="GTM-NGDM86GN" />
       <body className={`${montserrat.variable} ${openSans.variable} font-sans antialiased`}>
         {children}
       </body>
